32720933527a0428aa854b105b0c7d00e9275ad9
Taha Jadid <jadid.taha@waroom.com>
2024-04-19 18:47:32 +0100

    Refactor package

--- app/src/main/java/com/example/androidmigration_compose/screen/theme/Type.kt
+++ app/src/main/java/com/example/androidmigration_compose/screen/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.androidmigration_compose.ui.theme
+package com.example.androidmigration_compose.screen.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
